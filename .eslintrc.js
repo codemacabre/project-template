6f8b54e1c2ed2c797dac5aa54d8f6ab39869a2c8
Myles Lewando <myles.lewando@opendataservices.coop>
2023-07-20 09:39:10 +0100

    🔧 Add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,43 @@
 module.exports = {
-  extends: [
-    'standard'
-  ],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
   overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+        '*.{ts,js}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    },
     {
       files: ['*.html'],
       parser: '@html-eslint/parser',
       extends: ['plugin:@html-eslint/recommended']
     }
   ],
+  // ignorePatterns: [
+  //   '*.html'
+  // ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
   plugins: [
     '@html-eslint',
     'html'
   ],
   rules: {
-    '@html-eslint/indent': ['error', 2]
+    '@html-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-non-null-assertion': 'off'
   }
 }
